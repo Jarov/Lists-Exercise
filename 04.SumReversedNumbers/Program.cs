4a6b939fd36e8bc3dd9801f8d0993721e8a7f248
Yavor <qvalkov@gmail.com>
2017-02-01 12:57:22 +0200

    made an app that reverse digits of a number from a list and sum all numbers

---
+++ 04.SumReversedNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SumReversedNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            int sum = ReverseDIgitsAndSum(numbers);
+
+            Console.WriteLine(sum);
+        }
+
+        static int ReverseDIgitsAndSum(List<int> numbers)
+        {
+            int sum = 0;
+            for (int index = 0; index < numbers.Count; index++)
+            {
+                int currentNumber = numbers[index];
+                string reversedNumber = "";
+
+                while (currentNumber > 0)
+                {
+                    if (currentNumber % 10 != 0) reversedNumber += (currentNumber % 10).ToString();
+                    currentNumber /= 10;
+                }
+
+                sum += int.Parse(reversedNumber);
+            }
+
+            return sum;
+        } 
+    }
+}
