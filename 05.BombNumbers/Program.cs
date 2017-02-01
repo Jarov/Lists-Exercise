67d3e8ba75dfb8f04cc003d904fb3d5c34cd02bd
Yavor <qvalkov@gmail.com>
2017-02-01 17:57:23 +0200

    Added Bomb Numbers exercise (still need some work on it)

---
+++ 05.BombNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> numbersToRemove = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int index = 0; index < numbersToRemove.Count; index++)
+            {
+                numbers.RemoveAll(x => x == numbersToRemove[index]);
+            }
+
+            int sum = 0;
+
+            for (int index = 0; index < numbers.Count; index++)
+            {
+                sum += numbers[index];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
